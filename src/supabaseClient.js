6ace24a08491eae147ce4abd3741631adbd7296e
RihardsIvans <rihardsivans@outlook.com>
2025-12-14 23:02:01 +0200

    fix: add supabase env to github pages build

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_https://ybdvdennqfvxpfavyooq.supabase.co;
+const supabaseAnonKey = import.meta.env.VITE_sb_publishable_X6UyVC4vhn10FGqswpMDIg_s8XLXo2l;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
